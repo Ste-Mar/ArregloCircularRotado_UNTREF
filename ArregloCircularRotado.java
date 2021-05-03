de5a347dc0bd8398031a4f7991b9bffe37ffd825
S. Marcaccio <stefanomarcaccio@gmail.com>
2021-05-03 11:31:46 -0300

    ArregloCircularRotado

--- ArregloCircularRotado.java
+++ ArregloCircularRotado.java
@@ -1,12 +1,38 @@
 public class ArregloCircularRotado {
 
+    /**
+     * pre: Ingresar el arreglo circular ordenado y sin numeros repetidos que desea
+     * analizar
+     * <p>
+     * post: Retorna la menor cantidad de rotaciones posibles para volver a
+     * ordenarlo
+     * 
+     * @param array debe estar ordenado y no debe contener numeros repetidos
+     * @return retorna un entero que representa la cantidad de rotaciones necesarias
+     *         para obtener el arreglo ordenado naturalmente
+     */
     public int cantidadDeRotaciones(Integer[] array) {
 
         return cantidadDeRotaciones(array, 0, array.length - 1);
 
     }
 
-    public int cantidadDeRotaciones(Integer[] array, int posicionInicial, int posicionFinal) {
+    /**
+     * pre: Ingresar el arreglo circular ordenado y sin numeros repetidos. La
+     * 'posicionInicial' y 'posicionFinal' se modifican dentro del metodo,
+     * reduciendo su longitud en cada llamado recursivo.
+     * <p>
+     * post: Retorna la menor cantidad de rotaciones posibles para volver a
+     * ordenarlo
+     * 
+     * @param array           debe estar ordenado y no debe contener numeros
+     *                        repetidos
+     * @param posicionInicial posicion inicial de analisis del arreglo
+     * @param posicionFinal   posicion final de analisis del arreglo
+     * @return retorna un entero que representa la cantidad de rotaciones necesarias
+     *         para obtener el arreglo ordenado naturalmente
+     */
+    private int cantidadDeRotaciones(Integer[] array, int posicionInicial, int posicionFinal) {
 
         int rotaciones = 0;
 
